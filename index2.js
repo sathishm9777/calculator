723e024a4de97f5b4ac75d2de5db77a1ba54f2ac
sathishm9777 <sathishm9777@gmail.com>
2024-06-23 19:26:10 +0530

    Add files via upload

---
+++ index2.js
@@ -0,0 +1,14 @@
+function disValue(val){
+    document.getElementById("display").value =  document.getElementById("display").value + val
+
+}
+function clearDis(){
+    document.getElementById("display").value ="";
+}
+
+function calculate(){
+    var userInput = document.getElementById("display").value;
+    var result =eval(userInput);
+
+    document.getElementById("display").value = result;  
+}
